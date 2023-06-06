63d3e0ddc938887f665c7ae5ba25f470997e6364
Noor-Ismot <ara.noor.ismot@gmail.com>
2023-06-06 23:00:45 +0200

    learning

--- Static-webpage-react/index.js
+++ Static-webpage-react/index.js
@@ -1,1 +1,23 @@
-reactDOM.render()
+ReactDOM.render(<ul>
+    <li>Composable</li>
+    <li>Easy</li>
+</ul>, document.getElementById("list"))
+
+
+function MainContent() {
+    return (
+        <h1>Fun facts about React</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <MainContent />
+    </div>,
+    document.getElementById("heading")
+)
+
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "dummyText";
+document.getElementById("heading").append(h1);
